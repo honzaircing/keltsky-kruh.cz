749ee3b7b02cb487eefb25d724b47f0b6ed83ef9
Honza Ircing <honza@ircing.cz>
2023-05-01 19:39:01 +0200

    ticket

---
+++ src/components/Ticket.vue
@@ -0,0 +1,53 @@
+<template>
+  <section id="buy-tickets" class="section-with-bg">
+    <div class="container aos-init aos-animate" data-aos="fade-up">
+      <div class="section-header">
+        <h2>Vstupenky</h2>
+        <p></p>
+      </div>
+
+      <div class="row">
+        <div
+          class="col-lg-6 aos-init aos-animate"
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
+          <div class="card mb-5 mb-lg-0">
+            <div class="card-body">
+              <h5 class="card-title text-muted text-uppercase text-center">
+                Předprodej
+              </h5>
+              <h6 class="card-price text-center">400 KČ</h6>
+              <hr />
+              <div class="text-center">
+                <a
+                  type="button"
+                  class="btn"
+                  href="https://skotskehry.bzuco.cloud/acs/tickets/cs/tickets/38/#/38"
+                  target="_blank"
+                >
+                  Koupit
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="col-lg-6 aos-init aos-animate"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
+          <div class="card mb-5 mb-lg-0">
+            <div class="card-body">
+              <h5 class="card-title text-muted text-uppercase text-center">
+                Na místě
+              </h5>
+              <h6 class="card-price text-center">500 KČ</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Pro Tier -->
+    </div>
+  </section>
+</template>
